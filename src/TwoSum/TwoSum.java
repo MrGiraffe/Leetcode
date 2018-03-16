f4be2063c8bfbe0300bc6b64e4abb1f58b662650
44t <vp@VP.local>
2018-03-15 22:25:49 -0700

    Added two problems: - find majority; - missing number.

--- src/TwoSum/TwoSum.java
+++ src/TwoSum/TwoSum.java
@@ -1,4 +1,36 @@
 package TwoSum;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+
+    static public int[] findSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        Map<Integer, Integer> mp = new <Integer, Integer>HashMap();
+        for (int i = 0; i < nums.length; i++) {
+
+            if (mp.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = mp.get(target - nums[i]);
+                return result;
+            }
+
+            mp.put(nums[i], i);
+        }
+
+        return result;
+
+    }
+
+
+    public static void main(String args[]) {
+        int[] arr = {2, 3, 1, 7, 0, 12, 4, 5};
+
+        System.out.println(Arrays.toString(findSum(arr, 9)));
+
+    }
 }
