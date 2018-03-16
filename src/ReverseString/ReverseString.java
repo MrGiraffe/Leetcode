bf1f1a2d061e55d387fe2f5558edf3ef7f80dec1
44t <vp@VP.local>
2018-03-16 14:17:09 -0700

    Added problem Reverse String

--- src/ReverseString/ReverseString.java
+++ src/ReverseString/ReverseString.java
@@ -1,4 +1,30 @@
 package ReverseString;
 
 public class ReverseString {
+
+    static public String reverseString(String str) {
+
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        char[] arr = str.toCharArray();
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            char tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
+        }
+        return String.valueOf(arr);
+
+    }
+
+    public static void main(String args[]) {
+        String str = "hello";
+        System.out.println(reverseString(str));
+
+    }
 }
