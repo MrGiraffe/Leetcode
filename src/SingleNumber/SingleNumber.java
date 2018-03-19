388f3de8f8015553b7762881ec5218e5096dc99c
44t <vp@VP.local>
2018-03-18 22:34:19 -0700

    synchronisation error fixing

--- src/SingleNumber/SingleNumber.java
+++ src/SingleNumber/SingleNumber.java
@@ -1,4 +1,23 @@
 package SingleNumber;
 
+import java.util.Optional;
+
 public class SingleNumber {
+
+    public static Optional<Integer> singleNumber(int[] arr) {
+
+        int result = 0;
+        for (int i : arr) {
+            result ^= i;
+        }
+
+        return Optional.of(result);
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {1, 4, 0, 4, 1};
+        System.out.println(singleNumber(arr).get());
+    }
 }
+
+
