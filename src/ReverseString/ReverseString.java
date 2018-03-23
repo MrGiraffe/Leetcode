a46f961e70c7eb003b4bb235ff8fef0b142902b2
44t <vp@VP.local>
2018-03-22 19:27:22 -0700

    Updated Revers String

--- src/ReverseString/ReverseString.java
+++ src/ReverseString/ReverseString.java
@@ -5,23 +5,28 @@ public class ReverseString {
     static public String reverseString(String str) {
 
         // M. This check is not needed?
-        if (str.length() <= 1) {
-            return str;
-        }
+
+//        if (str.length() <= 1) {
+//            return str;
+//        }
 
         // M. Probably more efficient would be to allocate new array with the size str.size() and populate it.
         // Or maybe use a StringBuilder.
         char[] arr = str.toCharArray();
+        char[] arr1 = new char[str.length()];
         int i = 0;
         int j = str.length() - 1;
-        while (i < j) {
-            char tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
+        while (0 <= j) {
+//            char tmp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = tmp;
+
+            arr1[j] = arr[i];
             i++;
             j--;
         }
-        return String.valueOf(arr);
+
+        return String.valueOf(arr1);
 
     }
 
