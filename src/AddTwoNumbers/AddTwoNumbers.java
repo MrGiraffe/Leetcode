330402d1ac230e5dc333e4634990a1259288ac97
MrGiraffe <37452658+MrGiraffe@users.noreply.github.com>
2018-03-23 22:42:25 -0700

    Update AddTwoNumbers.java

--- src/AddTwoNumbers/AddTwoNumbers.java
+++ src/AddTwoNumbers/AddTwoNumbers.java
@@ -3,6 +3,7 @@ package AddTwoNumbers;
 public class AddTwoNumbers {
     public static class ListNode {
         int val;
+        // Use Optional<ListNode>
         ListNode next;
 
         ListNode(int num) {
@@ -14,14 +15,19 @@ public class AddTwoNumbers {
 
 
         ListNode ln = new ListNode(0), preLast = new ListNode(0);
+        // Declare one variable on one lime.
         ListNode ln1 = l1, ln2 = l2, curr = ln;
         int sum, buf = 0;
 
         while (!(ln1 == null) || !(ln2 == null)) {
 
+            // I think with Optional magic check for null should not be necessary. Figure out how it works.
+            // Wouldn't hurt to check the preconditions 0 <= val < 10. Use assert.
             sum = ((ln1 != null) ? ln1.val : 0) + ((ln2 != null) ? ln2.val : 0) + buf;
             if (sum >= 10) {
+                //  curr.val = isntead of +=. Same below. 
                 curr.val += (sum % 10);
+                // buf = 1; instead?
                 buf = sum / 10;
 
             } else {
@@ -31,6 +37,7 @@ public class AddTwoNumbers {
             }
             curr.next = new ListNode(0);
 
+            // Use optional.
             if (!(ln1 == null)) ln1 = ln1.next;
             if (!(ln2 == null)) ln2 = ln2.next;
 
@@ -45,6 +52,7 @@ public class AddTwoNumbers {
         return ln;
     }
 
+    // Try using JUnit test framework.
     public static void main(String args[]) {
         ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(8);
